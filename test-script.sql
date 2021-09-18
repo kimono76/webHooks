84ce1f7a0829dce219df72622089f2af71669c6e
AlbertoGruning-Fedora <alberto.gruning.zen@gmail.com>
2021-09-18 17:11:40 -0400

    sccript

--- test-script.sql
+++ test-script.sql
@@ -9,4 +9,12 @@ insert into fist_table (id,misc) values(2, 'this is another test')
 go
 select * from fist_table
 
+USE AirlineWebDB
+GO
 
+
+USE TravelAgentWebDB
+GO
+
+INSERT INTO dbo.SubscriptionSecrets(secret, publisher)
+VALUES('3934bdb5-7edc-43d2-87dd-ee6f98f71720', 'PanAustralian')
