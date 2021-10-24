9eac1e8bacf5c69d4e46f6272757cae7f05acc06
AlbertoGruning-lenovo-fedora35Server <kimono76.live@gmail.com>
2021-10-24 13:14:39 -0300

    script

--- test-script.sql
+++ test-script.sql
@@ -1,13 +1,3 @@
-create database dbTest 
-go
-use dbtest
-GO
---create table fist_table (id int not null,misc nvarchar(max),primary key (id))
-GO
-insert into fist_table (id,misc) values(1, 'this is a test')
-insert into fist_table (id,misc) values(2, 'this is another test')
-go
-select * from fist_table
 
 USE AirlineWebDB
 GO
@@ -18,3 +8,6 @@ GO
 
 INSERT INTO dbo.SubscriptionSecrets(secret, publisher)
 VALUES('3934bdb5-7edc-43d2-87dd-ee6f98f71720', 'PanAustralian')
+
+GO
+SELECT * FROM dbo.SubscriptionSecrets
